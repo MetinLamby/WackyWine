5f05e74ae50f024646b79b5a8a11a02a28f90db2
Metin Lamby <metin.lamby@outlook.com>
2019-04-09 16:27:57 +0200

    removed the filter btn

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,7 +1,10 @@
 class WinesController < ApplicationController
   def index
+    if params[:query].present?
+    else
     @white_wines =  Winetype.first.wines
     @red_wines = Winetype.second.wines
+    end
   end
 
   def show
